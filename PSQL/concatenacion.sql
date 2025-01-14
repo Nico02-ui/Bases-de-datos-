ff29390955a0af456e9b7c392089ff6a63d1a254
Nico <rodriguezguerranicolas@gmail.com>
2025-01-14 20:36:17 +0100

    V2 concatenacion.sql

--- PSQL/concatenacion.sql
+++ PSQL/concatenacion.sql
@@ -3,3 +3,10 @@ SELECT
     CONCAT(nombre, ' ', apellido1, ' ', apellido2) AS "Concatenado con CONCAT",
     apellido1 || ' ' || apellido2 || ' ' || nombre AS "Concatenado con ||"
 FROM investigador;
+
+SELECT 
+    nombre, 
+    CONCAT(nombre, ' ', apellido1, ' ', apellido2) AS "Concatenado con CONCAT",
+    CONCAT_WS(' ', nombre, apellido1, apellido2) AS "Concatenado con CONCAT_WS"
+FROM investigador;
+
